6a7cb75b480d9a81dba8665dbe93c38d2d7f90a6
xuwenkang <xwk1098941306@163.com>
2018-03-14 09:40:28 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    }
+}
